2fb67c9d00f41794085b5c807f4baa0faf1c4f4d
zhenya457 <zheka4573@gmail.com>
2025-11-08 20:02:25 +0200

    added new bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "epam-project-tfstate-61812246"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
